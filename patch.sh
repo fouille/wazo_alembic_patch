952c9e9589e16c61d474cec4af326ece76a0dc86
Franck M <franckisys@gmail.com>
2020-02-13 13:24:10 +0100

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # by Wazo Inc. Support
 
-echo "Quickfix over duplicate row in nestbox-confd alembic version"
+echo "Quickfix over duplicate row in alembic version"
   VERSION=$(sudo -u postgres psql asterisk -c "select * from alembic_version" -tA | uniq)
   sudo -u postgres psql asterisk -c "delete from alembic_version where version_num='$VERSION'";
   sudo -u postgres psql asterisk -c "insert into alembic_version (version_num) values ('$VERSION');";
